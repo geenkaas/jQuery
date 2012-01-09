ec073366e2f5963208e6d75f9eb280e908e4423b
Firstname LastnameGeen <github@geenkaas.nl>
2012-01-09 11:55:25 +0100

    added handlers for scroll, resize and load

--- document_ready.js
+++ document_ready.js
@@ -1,7 +1,34 @@
+	var scrolled;
 
-//	When page has finished loading
-$(document).ready(function() {
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Page initialisation and control	of screen	  												  //
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//	When document structure is loaded
+	$(document).ready(function() {
+		
+		//	Stuff to do
+		
+	});	//	End document ready
 
+//	What to do when page is loaded completely
+	$(window).load(function() {
+		
+		//	Stuff to do
+		
+	});	//	End window load
+
+//	When user scrolls
+	$(window).scroll(function() {
 	
-});	//	End document ready
+		//	Check how far user is scrolled
+			scrolled = parseInt($(window).scrollTop());
+			
+	});	//	End window scroll
+
+//	When user resizes window
+	$(window).resize(function() {
+		
+		//	Stuff to do
+				
+	});	//	End window resize
