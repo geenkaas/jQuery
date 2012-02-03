d67529be6e4e2bda87a32376487695c0db36b254
Firstname LastnameGeen <github@geenkaas.nl>
2012-02-03 10:22:39 +0100

    Updated Twitter feed plugin

--- twitter_feed.js
+++ twitter_feed.js
@@ -1,28 +1,6 @@
 //	Twitter feed
-//	Script
-function twitterCallback(tweets) {
-	var statusHTML = [];
-	for (var i=0; i<tweets.length; i++){
-		var username = tweets[i].user.screen_name;
-		var profile_image = tweets[i].user.profile_image_url;
-		var status = tweets[i].text.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
-			return '<a href="'+url+'">'+url+'</a>';
-		}).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
-			return  reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
-		});
-		statusHTML.push('<li><a href="http://twitter.com/'+username+'/"><img src="'+profile_image+'" /></a><p><span>'+status+'</span></p></li>');
-	};
-	document.getElementById('twitterfeed').innerHTML = statusHTML.join('');
-};
-
-//	HTML (find and replace REPLACE here)
-		<!-- Place this at location -->
-		<ul id="twitterfeed"><li>Twitter can not be loaded</li></ul>
 		
-		<!-- Place this just above the /body tag -->
-			<script src="http://twitter.com/statuses/user_timeline/REPLACE.json?callback=twitterCallback&count=10"></script>
-			
-//	CSS
+////	CSS
 ul#twitterfeed {
 	list-style: none;
 	margin: 0;
@@ -41,3 +19,39 @@ ul#twitterfeed img {
 ul#twitterfeed p {
 	margin: 0;
 	}
+	
+////	Script
+//	Loading Twitter JSON and building HTML output
+	function twitterCallback(tweets) {
+		var statusHTML = [];
+		for (var i=0; i<tweets.length; i++){
+			var username = tweets[i].user.screen_name;
+			var name = tweets[i].user.name;
+			var date = tweets[i].user.created_at;
+			var profile_image = tweets[i].user.profile_image_url;
+			var status = tweets[i].text.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
+				return '<a href="'+url+'">'+url+'</a>';
+			}).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
+				return	reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
+			});
+			statusHTML.push('<li><a href="http://twitter.com/'+username+'/"><h3>'+username+'</h3><h4>'+name+'</h4></a><p><span>'+status+'</span></p><h4>'+date+'</h4></li>');
+		};
+		if ($('#twitterfeed').length !== 0) {
+			document.getElementById('twitterfeed').innerHTML = statusHTML.join('');
+		};
+	};
+
+////	HTML
+	//	find and replace REPLACE were feed is to show)
+		<div id="twitterwrapper">
+			<ul id="twitterfeed">
+				<li>
+					<h3>Twitter is temporary over capacity</h3>
+				</li>
+			</ul>
+		</div>
+		
+	//	Place this just above the /body tag
+		<!-- Twitter -->
+			<script src="//platform.twitter.com/widgets.js"></script>
+			<script src="http://twitter.com/statuses/user_timeline/REPLACE.json?callback=twitterCallback&count=10"></script>
